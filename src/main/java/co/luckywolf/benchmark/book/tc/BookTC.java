ea32a32d57506a68be86236b7db00127f30905af
Rob Austin <rob.austin@boundedbuffer.co.uk>
2024-06-07 15:01:22 +0100

    added instrement

--- src/main/java/co/luckywolf/benchmark/book/tc/BookTC.java
+++ src/main/java/co/luckywolf/benchmark/book/tc/BookTC.java
@@ -45,31 +45,15 @@ public class BookTC implements BytesMarshallable, Marshallable {
 
     // bid
     public int bidCount = 0;
-    public double bidPrice0, bidPrice1, bidPrice2, bidPrice3, bidPrice4, bidPrice5, bidPrice6, bidPrice7, bidPrice8, bidPrice9,
-            bidPrice10, bidPrice11, bidPrice12, bidPrice13, bidPrice14, bidPrice15, bidPrice16, bidPrice17, bidPrice18, bidPrice19,
-            bidPrice20, bidPrice21, bidPrice22, bidPrice23, bidPrice24, bidPrice25, bidPrice26, bidPrice27, bidPrice28, bidPrice29,
-            bidPrice30, bidPrice31, bidPrice32, bidPrice33, bidPrice34, bidPrice35, bidPrice36, bidPrice37, bidPrice38, bidPrice39,
-            bidPrice40, bidPrice41, bidPrice42, bidPrice43, bidPrice44, bidPrice45, bidPrice46, bidPrice47, bidPrice48, bidPrice49;
+    public double bidPrice0, bidPrice1, bidPrice2, bidPrice3, bidPrice4, bidPrice5, bidPrice6, bidPrice7, bidPrice8, bidPrice9, bidPrice10, bidPrice11, bidPrice12, bidPrice13, bidPrice14, bidPrice15, bidPrice16, bidPrice17, bidPrice18, bidPrice19, bidPrice20, bidPrice21, bidPrice22, bidPrice23, bidPrice24, bidPrice25, bidPrice26, bidPrice27, bidPrice28, bidPrice29, bidPrice30, bidPrice31, bidPrice32, bidPrice33, bidPrice34, bidPrice35, bidPrice36, bidPrice37, bidPrice38, bidPrice39, bidPrice40, bidPrice41, bidPrice42, bidPrice43, bidPrice44, bidPrice45, bidPrice46, bidPrice47, bidPrice48, bidPrice49;
 
-    public double bidVolume0, bidVolume1, bidVolume2, bidVolume3, bidVolume4, bidVolume5, bidVolume6, bidVolume7, bidVolume8, bidVolume9,
-            bidVolume10, bidVolume11, bidVolume12, bidVolume13, bidVolume14, bidVolume15, bidVolume16, bidVolume17, bidVolume18, bidVolume19,
-            bidVolume20, bidVolume21, bidVolume22, bidVolume23, bidVolume24, bidVolume25, bidVolume26, bidVolume27, bidVolume28, bidVolume29,
-            bidVolume30, bidVolume31, bidVolume32, bidVolume33, bidVolume34, bidVolume35, bidVolume36, bidVolume37, bidVolume38, bidVolume39,
-            bidVolume40, bidVolume41, bidVolume42, bidVolume43, bidVolume44, bidVolume45, bidVolume46, bidVolume47, bidVolume48, bidVolume49;
+    public double bidVolume0, bidVolume1, bidVolume2, bidVolume3, bidVolume4, bidVolume5, bidVolume6, bidVolume7, bidVolume8, bidVolume9, bidVolume10, bidVolume11, bidVolume12, bidVolume13, bidVolume14, bidVolume15, bidVolume16, bidVolume17, bidVolume18, bidVolume19, bidVolume20, bidVolume21, bidVolume22, bidVolume23, bidVolume24, bidVolume25, bidVolume26, bidVolume27, bidVolume28, bidVolume29, bidVolume30, bidVolume31, bidVolume32, bidVolume33, bidVolume34, bidVolume35, bidVolume36, bidVolume37, bidVolume38, bidVolume39, bidVolume40, bidVolume41, bidVolume42, bidVolume43, bidVolume44, bidVolume45, bidVolume46, bidVolume47, bidVolume48, bidVolume49;
 
     // ask
     public int askCount;
-    public double askPrice0, askPrice1, askPrice2, askPrice3, askPrice4, askPrice5, askPrice6, askPrice7, askPrice8, askPrice9,
-            askPrice10, askPrice11, askPrice12, askPrice13, askPrice14, askPrice15, askPrice16, askPrice17, askPrice18, askPrice19,
-            askPrice20, askPrice21, askPrice22, askPrice23, askPrice24, askPrice25, askPrice26, askPrice27, askPrice28, askPrice29,
-            askPrice30, askPrice31, askPrice32, askPrice33, askPrice34, askPrice35, askPrice36, askPrice37, askPrice38, askPrice39,
-            askPrice40, askPrice41, askPrice42, askPrice43, askPrice44, askPrice45, askPrice46, askPrice47, askPrice48, askPrice49;
+    public double askPrice0, askPrice1, askPrice2, askPrice3, askPrice4, askPrice5, askPrice6, askPrice7, askPrice8, askPrice9, askPrice10, askPrice11, askPrice12, askPrice13, askPrice14, askPrice15, askPrice16, askPrice17, askPrice18, askPrice19, askPrice20, askPrice21, askPrice22, askPrice23, askPrice24, askPrice25, askPrice26, askPrice27, askPrice28, askPrice29, askPrice30, askPrice31, askPrice32, askPrice33, askPrice34, askPrice35, askPrice36, askPrice37, askPrice38, askPrice39, askPrice40, askPrice41, askPrice42, askPrice43, askPrice44, askPrice45, askPrice46, askPrice47, askPrice48, askPrice49;
 
-    public double askVolume0, askVolume1, askVolume2, askVolume3, askVolume4, askVolume5, askVolume6, askVolume7, askVolume8, askVolume9,
-            askVolume10, askVolume11, askVolume12, askVolume13, askVolume14, askVolume15, askVolume16, askVolume17, askVolume18, askVolume19,
-            askVolume20, askVolume21, askVolume22, askVolume23, askVolume24, askVolume25, askVolume26, askVolume27, askVolume28, askVolume29,
-            askVolume30, askVolume31, askVolume32, askVolume33, askVolume34, askVolume35, askVolume36, askVolume37, askVolume38, askVolume39,
-            askVolume40, askVolume41, askVolume42, askVolume43, askVolume44, askVolume45, askVolume46, askVolume47, askVolume48, askVolume49;
+    public double askVolume0, askVolume1, askVolume2, askVolume3, askVolume4, askVolume5, askVolume6, askVolume7, askVolume8, askVolume9, askVolume10, askVolume11, askVolume12, askVolume13, askVolume14, askVolume15, askVolume16, askVolume17, askVolume18, askVolume19, askVolume20, askVolume21, askVolume22, askVolume23, askVolume24, askVolume25, askVolume26, askVolume27, askVolume28, askVolume29, askVolume30, askVolume31, askVolume32, askVolume33, askVolume34, askVolume35, askVolume36, askVolume37, askVolume38, askVolume39, askVolume40, askVolume41, askVolume42, askVolume43, askVolume44, askVolume45, askVolume46, askVolume47, askVolume48, askVolume49;
 
 
     public double getBidPrice(int index) {
@@ -153,6 +137,7 @@ public class BookTC implements BytesMarshallable, Marshallable {
 
     @Override
     public void writeMarshallable(@NotNull WireOut wire) throws InvalidMarshallableException {
+        wire.write("instrument").text(ShortTextLongConverter.INSTANCE.asText(instrument));
         wire.write("bidCount").int32(bidCount);
         wire.write("askCount").int32(askCount);
         wire.write("bids").list(addBidRungs(bidCount), Rung.class);
@@ -161,6 +146,11 @@ public class BookTC implements BytesMarshallable, Marshallable {
 
     @Override
     public void readMarshallable(@NotNull WireIn wire) throws IORuntimeException {
+        instrument = wire.read("instrument").int64();
+        StringBuilder sb = new StringBuilder();
+        wire.read("instrument").text(sb);
+        instrument = ShortTextLongConverter.INSTANCE.parse(sb);
+
         bidCount = wire.read("bidCount").int32();
         askCount = wire.read("askCount").int32();
         bidCount = readRungs(wire, "bid", this::setAllBidRungs);
